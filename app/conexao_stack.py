2959d90d1f53496a81d5e68e6f162485f575493f
Thiago Villani <iamthiagovillani@gmail.com>
2022-09-14 09:29:22 -0300

    Add files via upload

--- app/conexao_stack.py
+++ app/conexao_stack.py
@@ -1,15 +1,12 @@
-from django import db
-
-
 # API OpenDataSUS
 api_url = "https://imunizacao-es.saude.gov.br/desc-imunizacao/_search"
-api_user = "imunizacao_public"
-api_pass = "qlto5t&7r_@+#Tlstigi"
+api_user = "xxxxxxxxxx"
+api_pass = "xxxxxxxxxxxxxx"
 api_auth = (api_user,api_pass)
 # DB_Mysql
-db_url = "db-covid19.cw6sufr4jljr.us-east-1.rds.amazonaws.com"
-db_user = "admin"
-db_pass = "thiago2022"
+db_url = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+db_user = "xxxxx"
+db_pass = "xxxxxx"
 db_port = "3306"
 db_database = "covid19"
 
